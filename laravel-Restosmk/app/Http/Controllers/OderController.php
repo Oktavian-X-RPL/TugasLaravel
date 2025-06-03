edb0f4d8f9ad7c003ba82251755c5e5ce06b7b0c
Oktavian-X-RPL <oktalinixxx88@gmail.com>
2025-06-03 07:51:25 +0700

    Selesai

---
+++ laravel-Restosmk/app/Http/Controllers/OderController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Oder;
+use App\Http\Requests\StoreOderRequest;
+use App\Http\Requests\UpdateOderRequest;
+
+class OderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreOderRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreOderRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Oder  $oder
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Oder $oder)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Oder  $oder
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Oder $oder)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateOderRequest  $request
+     * @param  \App\Models\Oder  $oder
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateOderRequest $request, Oder $oder)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Oder  $oder
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Oder $oder)
+    {
+        //
+    }
+}
---
+++ laravel-Restosmk/app/Http/Controllers/OderDetailController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OderDetail;
+use App\Http\Requests\StoreOderDetailRequest;
+use App\Http\Requests\UpdateOderDetailRequest;
+
+class OderDetailController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreOderDetailRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreOderDetailRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\OderDetail  $oderDetail
+     * @return \Illuminate\Http\Response
+     */
+    public function show(OderDetail $oderDetail)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\OderDetail  $oderDetail
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(OderDetail $oderDetail)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateOderDetailRequest  $request
+     * @param  \App\Models\OderDetail  $oderDetail
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateOderDetailRequest $request, OderDetail $oderDetail)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\OderDetail  $oderDetail
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(OderDetail $oderDetail)
+    {
+        //
+    }
+}
